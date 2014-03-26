660458eec091e7b0e994bfc006c37b63e6e92470
Benjamin Nørgaard <benjamin@webtoad.dk>
2014-03-26 21:45:08 +0100

    corrects SQL and now use system instead of the useless exec

--- mysql_backup.rb
+++ mysql_backup.rb
@@ -61,7 +61,7 @@ class MySQLBackup
     client = Mysql2::Client.new(host: 'localhost',
                                 username: Settings.mysql[:user],
                                 password: Settings.mysql[:pass])
-    client.query('SELECT SCHEMA_NAME FROM `information_schema`.`SCHEMATA` WHERE SCHEMA_NAME<>information_schema;', symbolize_keys: true).each do |row|
+    client.query('SELECT SCHEMA_NAME FROM `information_schema`.`SCHEMATA` WHERE SCHEMA_NAME<>"information_schema";', symbolize_keys: true).each do |row|
       @databases << row[:SCHEMA_NAME]
     end
   end
@@ -73,7 +73,7 @@ class MySQLBackup
     end
     FileUtils.mkdir dirname
     @databases.each do |db|
-      exec "mysqldump -u#{Settings.mysql[:user]} -p#{Settings.mysql[:pass]} #{db} | gzip > #{dirname}/#{db}.sql.gz}"
+      system "mysqldump -u#{Settings.mysql[:user]} -p#{Settings.mysql[:pass]} #{db} | gzip > #{dirname}/#{db}.sql.gz"
     end
   end
 end
